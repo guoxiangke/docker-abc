00acf48103706214dd100fe4e81534c3e4402e9c
Your Name <you@example.com>
2017-10-09 18:24:08 +0800

    no baseurl

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -581,10 +581,10 @@ $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
 # $conf['allow_authorize_operations'] = FALSE;
 // https://stackoverflow.com/questions/36222254/drupal-mixed-content-error-when-using-ssl-certificate
 $conf['reverse_proxy'] = TRUE;
-$base_url = 'https://'.$_SERVER['SERVER_NAME'];
+// $base_url = 'https://'.$_SERVER['SERVER_NAME'];
 // $base_url = 'https://abc.vultr.yongbuzhixi.com';
 // $base_url = 'https://abc.yongbuzhixi.com';
-// $base_url = 'https://www.abc-chinaedu.com';
+$base_url = 'https://www.abc-chinaedu.com';
 // $base_url = 'https://abc-chinaedu.com';
 $conf['reverse_proxy_addresses'] = array('172.17.0.7');
 $conf['reverse_proxy_header'] = 'HTTP_X_FORWARDED_FOR';
